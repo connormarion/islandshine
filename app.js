651a9e57d658f2713cb92c6b321b22920b2e89f0
Connor Marion <connormarion@connors-mbp.lan>
2019-12-05 16:41:25 -0500

    basic frames

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes/index');
+const subroutes = require('./routes/subroutes')
+const subdomain = require('express-subdomain');
+
+const app = express();
+app.set('view engine', 'ejs')
+app.use('/', routes)
+app.use('/public/', express.static('public/'))
+
+module.exports = app;
